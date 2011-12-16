0a70bd8b15d4c0855f5d4f679010a3206f3f02c3
Jim Hefferon <ftpmaint@alan.smcvt.edu>
2011-12-16 10:47:03 -0500

    add some cleary question

---
+++ rc.py
@@ -0,0 +1,17 @@
+import math
+days={1:31,  # Jan
+      2:29, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+BDAY=(7,12)
+max_res=0
+max_res_date=(-1,-1)
+for month in range(1,13):
+    for day in range(1,days[month]+1):
+        num=BDAY[0]*month+BDAY[1]*day
+        denom=math.sqrt(BDAY[0]**2+BDAY[1]**2)*math.sqrt(month**2+day**2)
+        if denom>0:
+            res=math.acos(min(num*1.0/denom,1))
+            if res>max_res:
+                max_res=res
+                max_res_date=(month,day)
+print "For ",str(BDAY),"the worst case is",str(res),"radians on date",str(max_res_date)
+print "  That is ",180*res/math.pi,"degrees"
