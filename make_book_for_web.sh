29ea0d19b3a5a1c7c4c84003f554abf73d7f7964
Jim Hefferon <ftpmaint@alan.smcvt.edu>
2012-02-23 13:38:52 -0500

    start search.tex

--- make_book_for_web.sh
+++ make_book_for_web.sh
@@ -8,12 +8,16 @@ mpost ch4.mp
 mpost ch5.mp
 mpost voting.mp
 mpost appen.mp
-pdflatex "\def\dvidrv{pdftex}\def\pbsifont{yes}\def\hrefout{yes}\input book"
+# pdflatex "\def\dvidrv{pdftex}\def\pbsifont{yes}\def\hrefout{yes}\input book"
+pdflatex "\def\hrefout{yes}\input book"
 makeindex -s book.isty -p odd book.idx
-pdflatex "\def\dvidrv{pdftex}\def\pbsifont{yes}\def\hrefout{yes}\input book"
+# pdflatex "\def\dvidrv{pdftex}\def\pbsifont{yes}\def\hrefout{yes}\input book"
+pdflatex "\def\hrefout{yes}\input book"
 # dvips -Pwww -obook.ps book
 
 # edit jhanswer.tex to set \input to bookans
-pdflatex "\def\dvidrv{pdftex}\def\pbsifont{yes}\def\hrefout{yes}\input jhanswer"
-pdflatex "\def\dvidrv{pdftex}\def\pbsifont{yes}\def\hrefout{yes}\input jhanswer"
+# pdflatex "\def\dvidrv{pdftex}\def\pbsifont{yes}\def\hrefout{yes}\input jhanswer"
+pdflatex "\def\hrefout{yes}\input jhanswer"
+# pdflatex "\def\dvidrv{pdftex}\def\pbsifont{yes}\def\hrefout{yes}\input jhanswer"
+pdflatex "\def\hrefout{yes}\input jhanswer"
 # dvips -Pwww -ojhanswer.ps jhanswer
