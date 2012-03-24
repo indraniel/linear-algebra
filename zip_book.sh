a5a0c8b89d05e2c37709490b3102ed21d47017ef
Jim Hefferon <ftpmaint@alan.smcvt.edu>
2012-03-24 09:53:28 -0400

    small edits

---
+++ zip_book.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Put all the needed files in a .zip for _Linear Algebra_ by Jim Hefferon
+#
+# 2012-Mar-12 JH
+
+zip book book/*.pdf
+zip book book/*.sh
+zip book book/*.tex
+zip book book/*.mp
+zip book book/*.sty
+zip book book/*.jpg 
+zip book book/*.png
+zip book book/1ST_README
+zip book book/Acknowledgements
+zip book book/TODO.org
+zip book book/*.dat
+zip book book/*.sage
