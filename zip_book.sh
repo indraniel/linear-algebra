52910cd2495e5c270c6bf6ea24ee0dab94c7fd46
Jim Hefferon <jhefferon@smcvt.edu>
2012-10-20 10:57:21 -0400

    correction to det intro

--- zip_book.sh
+++ zip_book.sh
@@ -4,15 +4,33 @@
 #
 # 2012-Mar-12 JH
 
-zip book book/*.pdf
-zip book book/*.sh
-zip book book/*.tex
-zip book book/*.mp
-zip book book/*.sty
-zip book book/*.jpg 
-zip book book/*.png
-zip book book/1ST_README
-zip book book/Acknowledgements
-zip book book/TODO.org
-zip book book/*.dat
-zip book book/*.sage
+zip linear-algebra linear-algebra/*.pdf
+zip linear-algebra linear-algebra/*.sh
+zip linear-algebra linear-algebra/*.tex
+zip linear-algebra linear-algebra/*.mp
+zip linear-algebra linear-algebra/*.sty
+zip linear-algebra linear-algebra/*.jpg 
+zip linear-algebra linear-algebra/*.png
+zip linear-algebra linear-algebra/1ST_README
+zip linear-algebra linear-algebra/Acknowledgements
+zip linear-algebra linear-algebra/TODO.org
+zip linear-algebra linear-algebra/*.dat
+zip linear-algebra linear-algebra/*.sage
+# slides
+zip linear-algebra linear-algebra/slides/README
+zip linear-algebra linear-algebra/slides/*.tex
+zip linear-algebra linear-algebra/slides/*.sty
+zip linear-algebra linear-algebra/slides/*.sage
+zip linear-algebra linear-algebra/slides/*.pdf
+zip linear-algebra linear-algebra/slides/*.sh
+zip linear-algebra linear-algebra/slides/asy/*
+# lab manual
+zip linear-algebra linear-algebra/lab/*.tex
+zip linear-algebra linear-algebra/lab/*.png
+zip linear-algebra linear-algebra/lab/*.sage
+zip linear-algebra linear-algebra/lab/*.cls
+zip linear-algebra linear-algebra/lab/*.sty
+zip linear-algebra linear-algebra/lab/fonts/*
+zip linear-algebra linear-algebra/lab/python/*.py
+zip linear-algebra linear-algebra/lab/python/*.sage
+zip linear-algebra linear-algebra/lab/sage-check/*.sage
