7ed32ec59a7965c1c0f8a41776652857a385bc05
Jim Hefferon <jhefferon@smcvt.edu>
2013-11-09 15:53:45 -0500

    get it to run on the fresh Linux Mint box

--- make_book_for_web.sh
+++ make_book_for_web.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #  Make the book for the web (hyperlinks, PDF, etc.)
-# mpost dotprod.mp
-# mpost ch1.mp
-# mpost ch2.mp
-# mpost ch3.mp
-# mpost ch4.mp
-# mpost ch5.mp
-# mpost voting.mp
-# mpost appen.mp
+mpost dotprod.mp
+mpost ch1.mp
+mpost ch2.mp
+mpost ch3.mp
+mpost ch4.mp
+mpost ch5.mp
+mpost voting.mp
+mpost appen.mp
 # # pdflatex "\def\dvidrv{pdftex}\def\pbsifont{yes}\def\hrefout{yes}\input book"
 pdflatex "\def\hrefout{yes}\input book"
 makeindex -s book.isty -p odd book.idx
@@ -17,7 +17,7 @@ pdflatex "\def\hrefout{yes}\input book"
 
 # edit jhanswer.tex to set \input to bookans
 # # pdflatex "\def\dvidrv{pdftex}\def\pbsifont{yes}\def\hrefout{yes}\input jhanswer"
-# pdflatex "\def\hrefout{yes}\input jhanswer"
+pdflatex "\def\hrefout{yes}\input jhanswer"
 # # pdflatex "\def\dvidrv{pdftex}\def\pbsifont{yes}\def\hrefout{yes}\input jhanswer"
-# pdflatex "\def\hrefout{yes}\input jhanswer"
+pdflatex "\def\hrefout{yes}\input jhanswer"
 # dvips -Pwww -ojhanswer.ps jhanswer
