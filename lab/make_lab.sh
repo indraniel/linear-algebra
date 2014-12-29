97f5a3c779a1f4367f5b1a0a91790ae6d2aa2851
Jim Hefferon <jhefferon@smcvt.edu>
2014-12-29 18:24:12 -0500

    get sagetex actions to work on lab

--- lab/make_lab.sh
+++ lab/make_lab.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 # make_lab.sh
 #  Bash shell script to regenerate the lab from source.
-# 2012-Dec-20 JH
+# 2014-Dec-29 JH Make sure you make sagetex known or the graphics won't 
+#   come out right-sized.
+#     http://www.sagemath.org/doc/installation/sagetex.html
+# 2012-Dec-20 JH Written
 
 # generate the asy graphics
 cd asy
@@ -16,5 +19,8 @@ bibtex lab
 xelatex --shell-escape lab
 
 # improve on some of the auto-generated graphics
-sage lab.sagetex.sage
-xelatex --shell-escape lab
+echo "\n\n\n+++++++++++++++++++++"
+echo "  LaTeX once more, this time using improved graphics only"
+echo "+++++++++++++++++++++"
+sage lab.sagetex.sage 2>&1 | tee make_lab_sage.out  # this file is created by Sage from sagesilent envs
+xelatex lab
